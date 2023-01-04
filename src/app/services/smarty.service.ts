9f991657be7c4add369d6c8e5f5aa833b21d9df0
Pavel Krotov <pavelkrotov@ppk-mbp-m1.local>
2023-01-04 17:10:36 -0500

    autoSecond is edited

--- src/app/services/smarty.service.ts
+++ src/app/services/smarty.service.ts
@@ -18,7 +18,7 @@ export class SmartyService {
   // autoUrlORIGINAL = 'https://us-autocomplete-pro.api.smartystreets.com/lookup?search=';
   // autoUrl = 'https://us-autocomplete-pro.api.smartystreets.com/lookup?search=';
   autoUrl = 'https://us-autocomplete-pro.api.smartystreets.com/lookup?key=' + this.vtlKey + '&search=';
-  aptUrl = 'https://us-autocomplete-pro.api.smartystreets.com/lookup';
+  aptUrl = 'https://us-autocomplete-pro.api.smartystreets.com/lookup' + this.vtlKey + '&search=';
   autoLookupUrl = 'https://us-autocomplete-pro.api.smartystreets.com/lookup?';
   autoLookupQuery = 'search=123+mai&include_only_cities=chicago%2Cil&include_only_states=mi&prefer_states=il';
   referer = '-H referer: http://localhost:4200';
@@ -49,15 +49,6 @@ export class SmartyService {
     // return this.http.get(`${this.refUrl}`);
   }
 
-  ontest() {
-    // return console.log(this.Lookup);
-  }
-
-  // autoStreet(term: string): any {
-  //   // return this.http.get<any>(`${this.autoUrl}${term}${this.vtlKey}&source=all`)
-  //   return this.http.get<any>(`${this.autoUrl}${term}&source=all`)
-  //     .pipe(map(data => data.suggestions))
-  // }
   autoStreet(term: string): any {
     return this.http.get<any>(`${this.autoUrl}${term}&source=all`)
         .pipe(map(data => data.suggestions))
@@ -70,8 +61,6 @@ export class SmartyService {
 
   autoSecond(val: any) {
     const ws = "";
-    const dws = " ";
-    const qq = '?search=1042+W+Center+St+Apt+A&selected=1042+W+Center+St+Apt+A+(24)+Orem+UT+84057';
 
     const {street_line, secondary, city, state, zipcode, entries} = val;
     const sl = street_line.replace(/\s/g, '+');
@@ -81,8 +70,23 @@ export class SmartyService {
     const zp = zipcode.replace(/\s/g, '+');
 
     const qq1 = '?search=' + street_line + ws + secondary + " " + city + ", " + state + " " + zipcode + '&selected=' + street_line + ws + secondary + " " + city + ", " + state + " " + zipcode;
-    // return this.http.get<any>(this.aptUrl + '?search=' + sl + '&selected=' + sl + '+' + sc + '+' + '(' + entries + ')' + '+' + cty + '+' + st + '+' + zp + this.authId + this.authToken)
-    return this.http.get<any>(this.aptUrl + '?search=' + '&key=' + this.vtlKey + sl + '&selected=' + sl + '+' + sc + '+' + '(' + entries + ')' + '+' + cty + '+' + st + '+' + zp )
+    return this.http.get<any>(this.aptUrl + sl + '&selected=' + sl + '+' + sc + '+' + '(' + entries + ')' + '+' + cty + '+' + st + '+' + zp + this.authId + this.authToken)
   }
+  // autoSecond(val: any) {
+  //   const ws = "";
+  //   const dws = " ";
+  //   const qq = '?search=1042+W+Center+St+Apt+A&selected=1042+W+Center+St+Apt+A+(24)+Orem+UT+84057';
+  //
+  //   const {street_line, secondary, city, state, zipcode, entries} = val;
+  //   const sl = street_line.replace(/\s/g, '+');
+  //   const sc = secondary.replace(/\s/g, '+');
+  //   const cty = city.replace(/\s/g, '+');
+  //   const st = state.replace(/\s/g, '+');
+  //   const zp = zipcode.replace(/\s/g, '+');
+  //
+  //   const qq1 = '?search=' + street_line + ws + secondary + " " + city + ", " + state + " " + zipcode + '&selected=' + street_line + ws + secondary + " " + city + ", " + state + " " + zipcode;
+  //   // return this.http.get<any>(this.aptUrl + '?search=' + sl + '&selected=' + sl + '+' + sc + '+' + '(' + entries + ')' + '+' + cty + '+' + st + '+' + zp + this.authId + this.authToken)
+  //   return this.http.get<any>(this.aptUrl + '?search=' + '&key=' + this.vtlKey + sl + '&selected=' + sl + '+' + sc + '+' + '(' + entries + ')' + '+' + cty + '+' + st + '+' + zp )
+  // }
 
 }
